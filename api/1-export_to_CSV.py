a709b01a8b53b11448f5617fd401a225838a981b
Katrolli <ennio_cano@live.com>
2023-04-05 17:14:39 +0200

    fetching

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""First api request"""
+import json
+import requests
+from sys import argv
+import csv
+
+
+if __name__ == "__main__":
+    """Script that makes api request"""
+
+    id = argv[1]
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos')
+
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(id))
+
+    todo_data = todos.text
+    user_data = user.text
+
+    curr_user = json.loads(user_data)
+    curr_todo = json.loads(todo_data)
+
+with open('{}.csv'.format(id), 'w', newline='') as file:
+    for items in curr_todo:
+        if items['userId'] == curr_user['id']:
+            ls = (id, curr_user['username'], str(
+                items['completed']), items['title'])
+            writer = csv.writer(file, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_ALL)
+            writer.writerow(ls)
